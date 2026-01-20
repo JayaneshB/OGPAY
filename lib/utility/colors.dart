e07db8d9d13e690ed704a44063f64e795490db84
JayaneshB <your.email@example.com>
2026-01-20 11:10:59 +0530

    colors and string file added

---
+++ lib/utility/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const emeraldGreen = Color(0xFF32A64F);
+  static const slateBlueGrey = Color(0xFF4F616A);
+  static const coralRed = Color(0xFFF25151);
+  static const amberYellow = Color(0xFFFFAE1F);
+
+  // Gradient
+  static const primaryGradient = LinearGradient(
+    colors: [Color(0xFF32A64F), Color(0xFF247E3B)],
+  );
+
+  // Grey Shades
+  static const coolGrey = Color(0xFF8F9BA1);
+  static const lightGrey = Color(0xFFC8C8C8);
+  static const extraLightGrey = Color(0xFFEAEAEA);
+  static const offWhite = Color(0xFFF8F8F8);
+}

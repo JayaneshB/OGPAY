c9f0aebdfd676d26aceeea417e7cea97258ca23e
Jenish <Jenish@fundsindia.com>
2026-01-20 11:16:21 +0530

    colour theme added

---
+++ lib/Utitlty/ThemeColours.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeColours {
+  static const Color green = Color(0xFF32A64F);
+  static const Color grey = Color(0xFF4F616A);
+  static const Color red = Color(0xFFF25151);
+  static const Color yellow = Color(0xFFFFAE1F);
+
+  static const Color grey100 = Color(0xFF8F9BA1);
+  static const Color grey200 = Color(0xFFC8C8C8);
+  static const Color grey300 = Color(0xFFEAEAEA);
+  static const Color grey400 = Color(0xFFF8F8F8);
+}
